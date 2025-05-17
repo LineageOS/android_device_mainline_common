ad994cd4447cef5d2c716689aa4a782c7b1ef9a9
Yumi Yukimura <me.cafebabe@gmail.com>
2025-05-18 03:45:28 +0800

    mainline/common: optional: Add tinyhal Audio HAL configuration
    Change-Id: I4a287e907ac6fdfafc49f59e8093f2e523dd1a8d

---
+++ optional/audio-hal_tinyhal/product.mk
@@ -0,0 +1,24 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+ifeq ($(TARGET_AUDIO_HAL),tinyhal)
+
+PRODUCT_PACKAGES += \
+    android.hardware.audio.service \
+    android.hardware.audio.effect@7.0-impl \
+    android.hardware.audio@7.1-impl \
+    audio.primary.tinyhal \
+    manifest_mainline_common_audio-hal_tinyhal.xml
+
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libeffects/data/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml
+
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.hardware.audio.primary=tinyhal
+
+PRODUCT_SOONG_NAMESPACES += \
+    external/tinyhal
+
+endif # TARGET_AUDIO_HAL
