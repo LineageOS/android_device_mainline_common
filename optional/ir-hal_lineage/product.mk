7ae46e72abdc4b440a4eed946332efb60c85fef0
Yumi Yukimura <me.cafebabe@gmail.com>
2025-05-19 01:44:47 +0800

    mainline/common: optional: Add Lineage IR HAL configuration
    Change-Id: I0128f393ac3e0412fef3a4a9ee45f94769134914

---
+++ optional/ir-hal_lineage/product.mk
@@ -0,0 +1,11 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+ifeq ($(TARGET_IR_HAL),lineage)
+
+PRODUCT_PACKAGES += \
+    android.hardware.ir-service.lineage
+
+endif # TARGET_IR_HAL
