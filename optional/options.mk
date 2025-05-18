8ecac2cb05acb7d5e043d6bf1a22e249606e9754
Yumi Yukimura <me.cafebabe@gmail.com>
2025-05-19 01:44:47 +0800

    mainline/common: optional: Add Lineage IR HAL configuration
    Change-Id: I0128f393ac3e0412fef3a4a9ee45f94769134914

--- optional/options.mk
+++ optional/options.mk
@@ -62,6 +62,14 @@ else
 TARGET_HEALTH_HAL ?= default-aidl
 endif
 
+# IR
+ifeq ($(TARGET_HAS_IR),true)
+ifneq ($(TARGET_INITIAL_BRINGUP),true)
+PRODUCT_PACKAGES += android.hardware.consumerir.prebuilt.xml
+TARGET_IR_HAL ?= lineage
+endif
+endif
+
 # Light
 TARGET_LIGHT_HAL ?= lineage
 
