5209a044b741b60da0be5e65b5c8c1ca225b3a04
Yumi Yukimura <me.cafebabe@gmail.com>
2025-07-13 01:21:46 +0800

    mainline/common: options.mk: Stop installing permission xmls
    * The original intention of this .mk file is just to set defaults
      of the options, not to do actual stuff
    * IR permission xml should be installed in APEX in next
    * USB accessory permission xml actually has the description [1],
      which does not stand for accessory mode.
    
    [1]: "This is the standard feature indicating that the device supports USB accessories."
    
    Change-Id: I003b9a350292cac4370efa3ee8923c92c899d80d

--- optional/options.mk
+++ optional/options.mk
@@ -66,7 +66,6 @@ endif
 
 # IR
 ifeq ($(TARGET_HAS_IR),true)
-PRODUCT_PACKAGES += android.hardware.consumerir.prebuilt.xml
 TARGET_IR_HAL ?= lineage
 endif
 
@@ -86,7 +85,6 @@ endif
 
 # USB
 ifneq ($(TARGET_SUPPORTS_USB_ACCESSORY_MODE),false)
-PRODUCT_PACKAGES += android.hardware.usb.accessory.prebuilt.xml
 TARGET_USB_GADGET_HAL ?= mainline
 TARGET_USB_HAL ?= lineage-basic
 TARGET_USB_INIT_SCRIPT ?= mainline
