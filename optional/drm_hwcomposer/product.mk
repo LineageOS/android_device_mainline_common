a07da204bfe22cae424adeff211973e4a76029d5
Yumi Yukimura <me.cafebabe@gmail.com>
2025-07-13 01:01:12 +0800

    mainline/common: drm_hwcomposer: Switch to APEX variant
    Change-Id: I9dd240ed41d9e61e15d51da0f239e5d49c4ce461

--- optional/drm_hwcomposer/product.mk
+++ optional/drm_hwcomposer/product.mk
@@ -8,7 +8,7 @@ ifeq ($(TARGET_GRAPHICS_COMPOSER_HAL),drm_hwcomposer)
 TARGET_DRM_HWCOMPOSER_HAL_INTERFACE ?= aidl
 ifeq ($(TARGET_DRM_HWCOMPOSER_HAL_INTERFACE),aidl)
 PRODUCT_PACKAGES += \
-    android.hardware.composer.hwc3-service.drm
+    com.android.hardware.graphics.composer.drm_hwcomposer
 else ifeq ($(TARGET_DRM_HWCOMPOSER_HAL_INTERFACE),libhardware)
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
