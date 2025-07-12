af8b4ba43c349a848afee5433fe1d57dc02e1e20
Yumi Yukimura <me.cafebabe@gmail.com>
2025-07-13 01:56:47 +0800

    mainline/common: swiftshader: Pack into APEX
    Change-Id: Id53fe3aee126beecaabbb17613977c68fd9c9bc5

--- optional/swiftshader/product.mk
+++ optional/swiftshader/product.mk
@@ -6,18 +6,9 @@
 ifeq ($(TARGET_GRAPHICS_VULKAN),swiftshader)
 
 PRODUCT_PACKAGES += \
-    vulkan.pastel
-
-PRODUCT_PACKAGES += \
-    android.hardware.vulkan.level-0.prebuilt.xml \
-    android.hardware.vulkan.version-1_0_3.prebuilt.xml \
-    android.software.vulkan.deqp.level-latest.prebuilt.xml
+    org.lineageos.device.mainline.common.graphics.vulkan.swiftshader
 
 # TODO(b/65201432): Swiftshader needs to create executable memory.
 PRODUCT_REQUIRES_INSECURE_EXECMEM_FOR_SWIFTSHADER := true
-
-PRODUCT_VENDOR_PROPERTIES += \
-    ro.cpuvulkan.version=4206592 \
-    ro.hardware.vulkan=pastel
 
 endif # TARGET_GRAPHICS_VULKAN
