3ad008859800f58810dfd6dba6e6aa3db92ffdcd
Yumi Yukimura <me.cafebabe@gmail.com>
2025-10-23 01:49:35 +0800

    mainline/common: drm_hwcomposer: Add support for using upstream variant
    Change-Id: I907dce2d33e89eeb94fdc1974f4eccbb0e363907

--- optional/drm_hwcomposer/product.mk
+++ optional/drm_hwcomposer/product.mk
@@ -5,22 +5,29 @@
 
 ifeq ($(TARGET_GRAPHICS_COMPOSER_HAL),drm_hwcomposer)
 
+TARGET_DRM_HWCOMPOSER_VARIANT ?= aosp
+ifeq ($(TARGET_DRM_HWCOMPOSER_VARIANT),upstream)
+LOCAL_SUFFIX := _upstream
+else
+LOCAL_SUFFIX :=
+endif
+
 TARGET_DRM_HWCOMPOSER_HAL_INTERFACE ?= aidl
 ifeq ($(TARGET_DRM_HWCOMPOSER_HAL_INTERFACE),aidl)
 ifeq ($(TARGET_DRM_HWCOMPOSER_INSIDE_APEX),true)
 PRODUCT_PACKAGES += \
-    com.android.hardware.graphics.composer.drm_hwcomposer
+    com.android.hardware.graphics.composer.drm_hwcomposer$(LOCAL_SUFFIX)
 else
 PRODUCT_PACKAGES += \
-    android.hardware.composer.hwc3-service.drm
+    android.hardware.composer.hwc3-service.drm$(LOCAL_SUFFIX)
 endif
 else ifeq ($(TARGET_DRM_HWCOMPOSER_HAL_INTERFACE),libhardware)
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
-    hwcomposer.drm \
+    hwcomposer.drm$(LOCAL_SUFFIX) \
     manifest_mainline_common_graphics-composer-hal_drm_hwcomposer_libhardware.xml
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.hardware.hwcomposer=drm
+    ro.hardware.hwcomposer=drm$(LOCAL_SUFFIX)
 else
 $(error Not supported)
 endif
