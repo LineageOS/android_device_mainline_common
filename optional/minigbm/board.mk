51a4117b5e78ec581136adf8cb8543cf7d38eccf
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-11-03 23:56:49 +0100

    mainline/common: Add support for minigbm gbm_mesa backend
    Change-Id: Ib4960381a9ca7efe30eec11d2cf8ea62ed0b8859

--- optional/minigbm/board.mk
+++ optional/minigbm/board.mk
@@ -5,6 +5,10 @@
 
 ifeq ($(TARGET_GRAPHICS_ALLOCATOR_HAL),minigbm)
 
+ifeq ($(TARGET_MINIGBM_PLATFORM),gbm_mesa)
+BOARD_MESA3D_BUILD_LIBGBM := true
+endif
+
 BOARD_VENDOR_SEPOLICY_DIRS += \
     external/minigbm/cros_gralloc/sepolicy \
     $(MAINLINE_COMMON_PATH)/sepolicy/vendor/minigbm
