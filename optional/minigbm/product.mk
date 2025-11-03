0bcf4d797ec7c2f5235641dc8835fe1a299853f3
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-11-03 23:56:49 +0100

    mainline/common: Add support for minigbm gbm_mesa backend
    Change-Id: Ib4960381a9ca7efe30eec11d2cf8ea62ed0b8859

--- optional/minigbm/product.mk
+++ optional/minigbm/product.mk
@@ -12,6 +12,12 @@ else
 LOCAL_MINIGBM_MODULE_SUFFIX := _$(TARGET_MINIGBM_PLATFORM)
 endif
 
+ifeq ($(TARGET_MINIGBM_PLATFORM),gbm_mesa)
+ifneq ($(TARGET_GRAPHICS),mesa)
+$(error TARGET_GRAPHICS=mesa is required when TARGET_MINIGBM_PLATFORM=gbm_mesa)
+endif # TARGET_GRAPHICS
+endif # TARGET_MINIGBM_PLATFORM
+
 TARGET_MINIGBM_HAL_INTERFACE ?= aidl
 ifeq ($(TARGET_MINIGBM_HAL_INTERFACE),aidl)
 PRODUCT_PACKAGES += \
@@ -37,6 +43,12 @@ PRODUCT_VENDOR_PROPERTIES += \
 endif
 else
 $(error Not supported)
+endif
+
+ifeq ($(TARGET_MINIGBM_PLATFORM),gbm_mesa)
+PRODUCT_PACKAGES += \
+    dri_gbm \
+    libgbm_mesa
 endif
 
 endif # TARGET_GRAPHICS_ALLOCATOR_HAL
