a858aa6dfd1b6017a4d02583c0b82f8d3c29f247
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-11-04 00:00:18 +0100

    mainline/common: Set minigbm platform in product
    Change-Id: Ifa87b5cfaee9f3e194ada57ed419977a51cb0a1f

--- optional/minigbm/board.mk
+++ optional/minigbm/board.mk
@@ -5,8 +5,6 @@
 
 ifeq ($(TARGET_GRAPHICS_ALLOCATOR_HAL),minigbm)
 
-$(call soong_config_set, minigbm, platform, $(TARGET_MINIGBM_PLATFORM))
-
 BOARD_VENDOR_SEPOLICY_DIRS += \
     external/minigbm/cros_gralloc/sepolicy \
     $(MAINLINE_COMMON_PATH)/sepolicy/vendor/minigbm
