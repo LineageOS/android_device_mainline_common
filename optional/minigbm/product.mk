378b68ef618ca3c10891910e2c1d5cfaf79dcfe5
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-11-08 04:08:24 +0100

    mainline/common: Fix minigbm AIDL configuration
    mesa3d will prefer using libhardware gralloc impl if it exists
    
    Change-Id: If140bfc3da96bba7747dbc3da732835fed912f1c

--- optional/minigbm/product.mk
+++ optional/minigbm/product.mk
@@ -13,14 +13,6 @@ LOCAL_MINIGBM_MODULE_SUFFIX := _$(TARGET_MINIGBM_PLATFORM)
 $(call soong_config_set, minigbm, platform, $(TARGET_MINIGBM_PLATFORM))
 endif
 
-PRODUCT_PACKAGES += \
-    gralloc.minigbm$(LOCAL_MINIGBM_MODULE_SUFFIX)
-
-ifneq ($(TARGET_MINIGBM_DO_NOT_SET_AS_DEFAULT),true)
-PRODUCT_VENDOR_PROPERTIES += \
-    ro.hardware.gralloc=minigbm$(LOCAL_MINIGBM_MODULE_SUFFIX)
-endif
-
 TARGET_MINIGBM_HAL_INTERFACE ?= aidl
 ifeq ($(TARGET_MINIGBM_HAL_INTERFACE),aidl)
 PRODUCT_PACKAGES += \
@@ -31,7 +23,13 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@4.0-service.minigbm$(LOCAL_MINIGBM_MODULE_SUFFIX) \
     android.hardware.graphics.mapper@4.0-impl.minigbm$(LOCAL_MINIGBM_MODULE_SUFFIX)
 else ifeq ($(TARGET_MINIGBM_HAL_INTERFACE),libhardware)
-$(error Not implemented yet)
+PRODUCT_PACKAGES += \
+    gralloc.minigbm$(LOCAL_MINIGBM_MODULE_SUFFIX)
+
+ifneq ($(TARGET_MINIGBM_DO_NOT_SET_AS_DEFAULT),true)
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.hardware.gralloc=minigbm$(LOCAL_MINIGBM_MODULE_SUFFIX)
+endif
 else
 $(error Not supported)
 endif
