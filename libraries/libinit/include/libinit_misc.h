0f3718c5784e6f8d6eb50dfe8f8fdc49ac1a8c9c
Yumi Yukimura <me.cafebabe@gmail.com>
2025-12-05 15:14:07 +0800

    mainline/common: libinit: Make it easy for us to enable debugging
    Change-Id: I8223d7b55d5493bd094320570e7f0cacf29de8ea

---
+++ libraries/libinit/include/libinit_misc.h
@@ -0,0 +1,9 @@
+/*
+ * Copyright (C) 2025 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+void enable_insecure_debugging(void);
--- libraries/libinit/libinit_mainline_common.cpp
+++ libraries/libinit/libinit_mainline_common.cpp
@@ -7,9 +7,16 @@
 #include <libinit_mainline_common.h>
 
 #include <libinit_dalvik_heap.h>
+#include <libinit_misc.h>
 #include <libinit_set_properties.h>
 
+#include <unistd.h>
+
 void vendor_load_properties_mainline_common(void) {
+    if (access("/metadata/.enable_insecure_debugging", F_OK) == 0) {
+        enable_insecure_debugging();
+    }
+
     set_dalvik_heap();
 #ifdef SET_PROPERTIES_FROM_DEVICETREE
     set_properties_from_devicetree();
