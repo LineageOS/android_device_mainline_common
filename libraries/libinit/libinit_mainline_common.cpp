d0127bd68836d91eb3dbab4f995899e8865fae28
Yumi Yukimura <me.cafebabe@gmail.com>
2025-12-05 15:14:07 +0800

    mainline/common: libinit: Add `enable_insecure_debugging()`
    Change-Id: I8223d7b55d5493bd094320570e7f0cacf29de8ea

--- libraries/libinit/libinit_mainline_common.cpp
+++ libraries/libinit/libinit_mainline_common.cpp
@@ -7,9 +7,11 @@
 #include <libinit_mainline_common.h>
 
 #include <libinit_dalvik_heap.h>
+#include <libinit_misc.h>
 #include <libinit_set_properties.h>
 
 void vendor_init_mainline_common(void) {
+    // enable_insecure_debugging(); // Commented out by default intentionally
     set_dalvik_heap();
 #ifdef SET_PROPERTIES_FROM_DEVICETREE
     set_properties_from_devicetree();
---
+++ libraries/libinit/libinit_misc.cpp
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2025 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <libinit_misc.h>
+
+#include <libinit_utils.h>
+
+#include <string>
+#include <unordered_map>
+
+void enable_insecure_debugging(void) {
+    const std::unordered_map kPropsToSet<std::string, std::string> = {
+            {"ro.adb.secure", "0"},
+            {"ro.boot.verifiedbootstate", "orange"},
+            {"ro.debuggable", "1"},
+            {"ro.secure", "0"},
+    };
+
+    for (const auto& [key, value] : kPropsToSet) {
+        property_override(key, value);
+    }
+}
