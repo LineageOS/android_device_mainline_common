a8a63c01e4d60b4e3292e11d19f83b2f61f6caad
Yumi Yukimura <me.cafebabe@gmail.com>
2025-12-05 21:43:35 +0800

    mainline/common: minigbm-upstream: Add support for gbm_mesa driver
    Co-authored-by: Sebastiano Barezzi <seba@sebaubuntu.dev>
    Change-Id: I811c64a1468439807c8f63a1408264239f575b03

--- optional/minigbm-upstream/product.mk
+++ optional/minigbm-upstream/product.mk
@@ -5,6 +5,22 @@
 
 ifeq ($(TARGET_GRAPHICS_ALLOCATOR_HAL),minigbm-upstream)
 
+ifeq ($(TARGET_MINIGBM_PLATFORM),gbm_mesa)
+TARGET_MINIGBM_PLATFORM :=
+TARGET_MINIGBM_UPSTREAM_ENABLE_GBM_MESA_DRIVER := true
+endif
+
+ifeq ($(TARGET_MINIGBM_UPSTREAM_ENABLE_GBM_MESA_DRIVER),true)
+ifneq ($(TARGET_GRAPHICS),mesa)
+$(error TARGET_GRAPHICS=mesa is required when TARGET_MINIGBM_UPSTREAM_ENABLE_GBM_MESA_DRIVER=true)
+endif
+PRODUCT_PACKAGES += \
+    dri_gbm \
+    libgbm_mesa
+TARGET_MINIGBM_UPSTREAM_INSIDE_APEX := false
+$(call soong_config_set_bool,minigbm_upstream,enable_gbm_mesa_driver,true)
+endif
+
 TARGET_MINIGBM_UPSTREAM_INSIDE_APEX ?= true
 ifeq ($(TARGET_MINIGBM_UPSTREAM_INSIDE_APEX),true)
 PRODUCT_PACKAGES += \
