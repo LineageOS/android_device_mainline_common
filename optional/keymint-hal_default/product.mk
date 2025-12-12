ab504a5cbc391935c2962ff28a06329f0bb00818
Yumi Yukimura <me.cafebabe@gmail.com>
2025-12-12 15:41:29 +0800

    mainline/common: keymint-hal_default: Add & default to APEX
    WARNING: This requires formatting userdata, set this to false
    if you don't want to require that on your target
    
    Change-Id: I8df1fd7260492d9faaa31722230835fae039debc

--- optional/keymint-hal_default/product.mk
+++ optional/keymint-hal_default/product.mk
@@ -5,7 +5,13 @@
 
 ifeq ($(TARGET_SECURITY_KEYMINT_HAL),default)
 
+TARGET_KEYMINT_HAL_DEFAULT_INSIDE_APEX ?= true
+ifeq ($(TARGET_KEYMINT_HAL_DEFAULT_INSIDE_APEX),true)
+PRODUCT_PACKAGES += \
+    com.android.hardware.keymint.rust_nonsecure
+else
 PRODUCT_PACKAGES += \
     android.hardware.security.keymint-service
+endif
 
 endif # TARGET_SECURITY_KEYMINT_HAL
