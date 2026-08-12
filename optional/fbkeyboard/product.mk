02c82b95dd0b0f7b75c8a377d561254626613fc2
Yumi Yukimura <me.cafebabe@gmail.com>
2026-08-12 18:24:50 +0800

    mainline/common: optional: Add fbkeyboard
    Change-Id: I7976e85b96a7e964121eb28c3a510d1ef89b6a26

---
+++ optional/fbkeyboard/product.mk
@@ -0,0 +1,11 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+ifeq ($(TARGET_ENABLE_FBKEYBOARD),true)
+
+PRODUCT_PACKAGES_DEBUG += \
+    fbkeyboard
+
+endif # TARGET_ENABLE_FBKEYBOARD
