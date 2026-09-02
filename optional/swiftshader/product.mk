562a09bf087188c8ea1db42ba717e1677b323139
Aaron Kling <webgeek1234@gmail.com>
2026-09-02 14:48:53 -0500

    mainline/common: swiftshader: Declare Vulkan 1.4 support
    Required by vts and swiftshader does support it on Android 17. For some
    reason, this is not available as a prebuilt package when 1.3 is; so this
    had to be changed to a copy files entry.
    
    Change-Id: Ia547593d41dc19070be3f09ae1b1dfa1d90ba307

--- optional/swiftshader/product.mk
+++ optional/swiftshader/product.mk
@@ -8,6 +8,9 @@ ifeq ($(TARGET_GRAPHICS_VULKAN),swiftshader)
 PRODUCT_PACKAGES += \
     org.lineageos.device.graphics.vulkan.swiftshader
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.vulkan.version-1_4.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml
+
 # TODO(b/65201432): Swiftshader needs to create executable memory.
 PRODUCT_REQUIRES_INSECURE_EXECMEM_FOR_SWIFTSHADER := true
 
