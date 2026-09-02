962bb6d50bc63e7c3f98b700945ccc19b3bcbee5
Aaron Kling <webgeek1234@gmail.com>
2026-09-02 14:51:00 -0500

    mainline/common: swiftshader: Enable skiavk
    Required by vts and supported by swiftshader
    
    Change-Id: I6c8dea6782ce58a2b2ab82a032f2e3d2a5a455fa

--- optional/swiftshader/product.mk
+++ optional/swiftshader/product.mk
@@ -14,4 +14,6 @@ PRODUCT_COPY_FILES += \
 # TODO(b/65201432): Swiftshader needs to create executable memory.
 PRODUCT_REQUIRES_INSECURE_EXECMEM_FOR_SWIFTSHADER := true
 
+TARGET_USES_VULKAN := true
+
 endif # TARGET_GRAPHICS_VULKAN
