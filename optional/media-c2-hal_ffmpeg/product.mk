5f372f1bd3c439275c64e6a4f0c16639e8da58d0
Yumi Yukimura <me.cafebabe@gmail.com>
2026-09-12 14:51:25 +0800

    mainline/common: media-c2-hal_ffmpeg: Import configuration files
    From https://github.com/raspberry-vanilla/android_external_ffmpeg_codec2/commit/71ac744017cc31d13a72e5f376a8d76b51aebcd2
    
    Change-Id: I980a578ae86cf3597d0d60cf61841a6e66304158

--- optional/media-c2-hal_ffmpeg/product.mk
+++ optional/media-c2-hal_ffmpeg/product.mk
@@ -8,4 +8,8 @@ ifeq ($(TARGET_MEDIA_C2_HAL),ffmpeg)
 PRODUCT_PACKAGES += \
     com.android.hardware.media.c2.ffmpeg
 
+PRODUCT_PACKAGES += \
+    android.hardware.media.c2-ffmpeg.policy \
+    media_codecs_ffmpeg_c2.xml
+
 endif # TARGET_MEDIA_C2_HAL
